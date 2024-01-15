51f4ab56405fec90afdff341d727df7691b71607
bohdan145 <bogdan.didchenko@wesoftyou.com>
2024-01-15 17:37:54 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useRedirectToStore } from "./utils";
 
 function App() {
+  useRedirectToStore();
+
   return (
     <div className="App">
       <header className="App-header">
---
+++ src/utils.js
@@ -0,0 +1,64 @@
+import { useEffect } from "react";
+
+/**
+ * Determine the mobile operating system.
+ * This function returns one of 'iOS', 'Android', 'Windows Phone', or 'unknown'.
+ *
+ * @returns {'iOS' | 'Android' | 'Windows Phone' | 'unknown'}
+ */
+function getMobileOperatingSystem() {
+  var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+
+  // Windows Phone must come first because its UA also contains "Android"
+  if (/windows phone/i.test(userAgent)) {
+    return "Windows Phone";
+  }
+
+  if (/android/i.test(userAgent)) {
+    return "Android";
+  }
+
+  // iOS detection from: http://stackoverflow.com/a/9039885/177710
+  if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+    return "iOS";
+  }
+
+  return "unknown";
+}
+
+function redirectTo() {
+  const OS = getMobileOperatingSystem();
+
+  switch (OS) {
+    case "iOS":
+      window.location.href =
+        "https://apps.apple.com/us/app/zillagigs/id1577376464";
+      break;
+    case "Android":
+      window.location.href =
+        "https://apps.apple.com/us/app/zillagigs/id1577376464";
+      break;
+    default:
+      window.location.href =
+        "https://apps.apple.com/us/app/zillagigs/id1577376464";
+  }
+}
+
+export const useRedirectToStore = () => {
+  useEffect(() => {
+    async function handleRedirect() {
+      try {
+        // if ("clipboard" in navigator)
+        await navigator.clipboard.writeText("HELLO FROM ZILLA");
+        // redirectTo();
+      } catch (e) {
+        console.log(e);
+        redirectTo();
+      }
+    }
+
+    setTimeout(() => {
+      handleRedirect();
+    }, 1000);
+  }, []);
+};
